ea492d08b9c546fef2e8b760ddcaaeb58b2dd8c6
Shrusti Kalshetty (Ext) <shrusti.kalshetty@tieto.com>
2026-04-02 11:57:41 +0530

    fixed haeder file

--- encoder.h
+++ encoder.h
@@ -1,6 +1,5 @@
 #ifndef ENCODER_H
 #define ENCODER_H
-
 #include <stdint.h>
 
 #define MAX_MAC_CE_SIZE 255
@@ -8,34 +7,38 @@
 #define SUCCESS 0
 #define FAILURE -1
 
-// LCID
-#define LCID_SHORT_BSR        61
-#define LCID_PHR              57
-#define LCID_CRNTI            58
-#define LCID_REC_BIT_RATE     53
-#define LCID_DSR              228
+typedef struct
+{
+    uint8_t MPE;
+    uint8_t R2;
+    uint8_t P;
+    uint8_t R;
+    uint8_t BT;
+    uint8_t X;
+} Flags;
 
-#define LCID_ENH_PHR          221
-#define LCID_SL_LBT           222
-#define LCID_ENH_BFR          235
-#define LCID_EXT_BSR          245
+#define LCID_SHORT_BSR 61
+#define LCID_PHR 57
+#define LCID_CRNTI 58
+#define LCID_REC_BIT_RATE 53
+#define LCID_DSR 228
+#define LCID_ENH_PHR 221
+#define LCID_SL_LBT 222
+#define LCID_ENH_BFR 235
+#define LCID_EXT_BSR 245
 
-// Functions
+// Functions 
 int short_bsr(uint8_t *pdu, int *offset, int argc, int lcg, int buffer);
-int phr(uint8_t *pdu, int *offset, int argc, int ph, int pcmax);
+int phr(uint8_t *pdu, int *offset, int argc, int ph, int pcmax, Flags flags);
 int crnti(uint8_t *pdu, int *offset, int argc, int value);
-int rec_bit_rate(uint8_t *pdu, int *offset, int argc, int lcid, int rate, int ul_dl);
-int dsr(uint8_t *pdu, int *offset, int argc, int *params);
-
-
-int enhanced_phr(uint8_t *pdu, int *offset, int ph1, int ph2, int pcmax);
+int rec_bit_rate(uint8_t *pdu, int *offset, int argc, int lcid, int rate, int ul_dl, Flags flags);
+int dsr(uint8_t *pdu, int *offset, int argc, int *params, Flags flags);
+int enhanced_phr(uint8_t *pdu, int *offset, int argc, int *params);
 int sl_lbt(uint8_t *pdu, int *offset, int value);
 int enhanced_bfr(uint8_t *pdu, int *offset, int argc, int *params);
 int extended_bsr(uint8_t *pdu, int *offset, int lcg, int buffer);
-
 int parse_and_encode(const char *filename, uint8_t *pdu, int *pdu_size);
 
-// Utils
 void print_hex(uint8_t *data, int len);
 void print_bits(uint8_t *data, int len);
 void add_padding(uint8_t *buffer, int *offset, int remaining);
